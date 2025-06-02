53c755f788a206f7f5579186f2d7ce3e4e453bc8
ganshyamsharma <gsmsharma99@gmail.com>
2025-06-02 07:31:23 +0530

    debounce circuit code

---
+++ debouncer.v
@@ -0,0 +1,14 @@
+module debouncer(
+	input i_clk, i_sig,
+	output o_ sig
+	);
+	reg q0 = 0, q1 = 0;
+	
+	assign o_sig = q0 & (~q1);
+	
+	always @(posedge i_clk) begin
+		q0 <= i_sig;
+		q1 <= q0;
+	end
+	
+	endmodule
